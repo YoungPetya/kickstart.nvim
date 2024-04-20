325776458be1733163882bcf20a7b87dc66af9e6
Young Petya <peter.pavicic@gmail.com>
2024-04-20 19:54:27 +0200

    added treesitter requirements to parse R files for R.nvim

--- init.lua
+++ init.lua
@@ -819,7 +819,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'r', 'markdown', 'rnoweb' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
